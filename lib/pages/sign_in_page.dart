97157009925c85b59d48c3314802207ba5f52e2e
Julian Columbres <julian.columbres@gmail.com>
2021-11-27 12:33:01 -0800

    firebase sign in and register

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -1,13 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import 'email_sign_in_form.dart';
+enum EmailSignInFormType { signIn, register }
 
 class SignInPage extends StatelessWidget {
-  const SignInPage({Key? key, required this.onSignIn}) : super(key: key);
+  SignInPage({Key? key, required this.onSignIn}) : super(key: key);
 
   final void Function(User) onSignIn;
-  Future<void> _signInAnonymously() async {
+
+  Future<void> signInAnonymously() async {
     try {
       final userCredentials = await FirebaseAuth.instance.signInAnonymously();
       onSignIn(userCredentials.user!);
@@ -16,36 +17,97 @@ class SignInPage extends StatelessWidget {
     }
   }
 
+  Future<void> signIn(String email, String password) async {
+    try {
+      final userCredentials = await FirebaseAuth.instance.signInWithCredential(
+          EmailAuthProvider.credential(email: email, password: password));
+      onSignIn(userCredentials.user!);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> register(String email, String password) async {
+    try {
+      final userCredentials =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      onSignIn(userCredentials.user!);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  final TextEditingController _emailController = new TextEditingController();
+  final TextEditingController _passwordController = new TextEditingController();
+
+  // void _submit() {
+  //   print(
+  //       'email: ${_emailController.text}, password: ${_passwordController.text}');
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Save It'),
       ),
-      // body: Padding(
-      //   padding: const EdgeInsets.all(20.0),
-      //   child: Card(
-      //     child: EmailSignInForm(),
-      //   ),
-      // ),
-      // body: ElevatedButton(
-      //   child: Text('Test Sign in'),
-      //   onPressed: () {
-      //     _signInAnonymously();
-      //   },
-      // ),
       body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            EmailSignInForm(),
-            ElevatedButton(
-              child: Text('Test Sign in'),
-              onPressed: () {
-                _signInAnonymously();
-              },
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // EmailSignInForm(),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              children: _buildChildren(),
             ),
-          ]),
+          ),
+          ElevatedButton(
+            child: Text('Test Sign in'),
+            onPressed: () {
+              signInAnonymously();
+            },
+          ),
+        ],
+      ),
     );
+  }
+
+  List<Widget> _buildChildren() {
+    return [
+      TextField(
+        controller: _emailController,
+        decoration: InputDecoration(
+          labelText: 'Email',
+          hintText: 'email@email.com',
+        ),
+      ),
+      SizedBox(height: 10.0),
+      TextField(
+        controller: _passwordController,
+        decoration: InputDecoration(
+          labelText: 'Password',
+        ),
+        obscureText: true,
+      ),
+      SizedBox(height: 20.0),
+      ElevatedButton(
+        child: Text('Sign in'),
+        onPressed: () {
+          signIn(_emailController.text, _passwordController.text);
+        },
+      ),
+      ElevatedButton(
+        child: Text('Register'),
+        onPressed: () {
+          register(_emailController.text, _passwordController.text);
+        },
+      )
+    ];
   }
 }
