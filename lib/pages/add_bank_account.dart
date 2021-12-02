814ad4eacd0d1e55dc0d8959e7f232af740f48c7
JianBin Wu <jianbin.wu@sjsu.edu>
2021-12-01 23:47:00 -0800

    add profile_controller.dart

--- lib/pages/add_bank_account.dart
+++ lib/pages/add_bank_account.dart
@@ -1,4 +1,3 @@
-import 'package:budget_tracker_ui/json/create_budget_json.dart';
 import 'package:budget_tracker_ui/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
